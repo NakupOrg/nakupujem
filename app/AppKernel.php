02390416defdfa7aecf4dd4a132f9c560599524d
takacm <michal.takac@keyup.eu>
2014-09-01 14:17:59 +0200

    Preklady

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Nakupujem\ShopBundle\NakupujemShopBundle(),
+            new Padam87\SearchBundle\Padam87SearchBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
